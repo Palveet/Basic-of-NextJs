9e728c893d3342cdb490a59a58a704fc2fcc0664
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-07-27 23:45:02 +0530

    next js started

---
+++ ninjalist/app/about.js
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      Lorem ipsum odor amet, consectetuer adipiscing elit. Hac massa ipsum curabitur est ut, feugiat torquent nunc. Mauris maximus tincidunt natoque in sociosqu elit iaculis. Consectetur quis nam maximus scelerisque dolor penatibus eleifend magnis sagittis. Per montes lacus purus lobortis dapibus ante convallis mattis condimentum. Consectetur dapibus mi imperdiet dui; dictum duis urna et. Interdum primis mauris gravida sit sollicitudin sed malesuada.
+
+Duis penatibus mi etiam ornare lacinia ullamcorper pulvinar cras. Congue purus molestie aliquet tristique taciti; odio leo ac. Leo magnis sagittis malesuada nunc nam magna. Potenti adipiscing inceptos rhoncus maximus odio quisque. Nunc mattis tempor aliquam mus varius curae viverra. Magnis fusce ultrices ultricies porttitor in dolor facilisis? Aliquam congue rutrum parturient, massa vehicula consequat dignissim risus. Accumsan amet penatibus sem; vitae torquent ex. Aluctus ipsum, elit risus torquent vehicula himenaeos.
+
+Amet ultrices habitasse at nisi accumsan vehicula suspendisse eu. Nec torquent dictumst sit est suspendisse non neque eget lorem. Mus rhoncus hac vel tellus quis semper varius. Sollicitudin dictumst ultrices habitant pharetra felis magna vitae consectetur phasellus. Dolor malesuada sed pulvinar neque pellentesque amet. Adipiscing porta efficitur dictum eros id nibh nam viverra. Egestas integer ornare ullamcorper tortor cras.
+
+Pretium ultricies ad convallis sociosqu mollis a montes quam. Platea erat a amet feugiat per. Pretium nascetur venenatis proin non vivamus; lacinia lacinia. Cubilia vivamus ad porttitor maecenas vel. Himenaeos auctor velit sit; curabitur vehicula sollicitudin feugiat. Suspendisse feugiat malesuada proin quisque leo eros. Venenatis elit dis commodo, condimentum ac primis nostra urna. Vel litora fermentum nam, ullamcorper porta sodales iaculis. Nisi ante platea tortor tortor massa, nisl eget.
+
+Nullam condimentum ex cursus lacus nascetur cursus. Dictumst mi sagittis ac nibh sagittis dui faucibus. Parturient nascetur ornare rutrum neque integer ultricies curabitur malesuada in. Vehicula sociosqu nostra vel cubilia conubia fringilla est. Habitasse sociosqu nunc tempus auctor feugiat pharetra nunc. Nullam nulla nascetur penatibus vitae eleifend erat cursus facilisis. Pretium dis libero eleifend maecenas pellentesque id suscipit. Senectus imperdiet ullamcorper proin tempus varius imperdiet. Porta urna ultricies integer nulla risus quam platea.
+    </div>
+  );
+}
---
+++ ninjalist/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Egestas lobortis mollis dis, erat platea varius. Semper fames ultricies vehicula parturient lacus erat blandit leo. Nostra vestibulum torquent natoque iaculis arcu aptent dapibus lectus. Suspendisse taciti ac imperdiet accumsan inceptos. Arcu facilisis per litora dictum lorem tristique inceptos ultricies. Sapien ultrices sed massa conubia fusce faucibus. Netus est a viverra nunc sodales.</p>
+    </div>
+  );
+}
